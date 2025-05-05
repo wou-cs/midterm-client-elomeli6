e2aa18a3bfbd958ea6bbf3222626fd7b5d67e1bf
YTMini <minitech@YTMinis-Mac-mini.local>
2025-05-04 18:20:56 -0700

    created function 3 but ran out of time

--- client.py
+++ client.py
@@ -36,4 +36,14 @@ def get_full_name_from_first(first_name):
     :param first_name:
     :return: A string containing the first and last name of the first programmer in the list of matches.
     """
+    try:
+        response = requests.get(f"http://chrisbrooks.pythonanywhere.com/api/programmers/{first_name}")
+        if response.status_code == 200:
+            data = response.json()
+            for name in data["programmers"]:
+                if name["first_name"] == first_name:
+                    return f"{name["first_name"] + name["last_name"]}"
+    except Exception:
+        return {}
+
     return ""
